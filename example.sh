f9542c1c72c55c590aa14b9b6ab966b38ad9373a
Damiano Braga <damiano.braga@gmail.com>
2014-11-01 12:56:10 -0700

    Added example for thoth

---
+++ example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+base_dir=`pwd`
+# Start activeMQ first
+echo "Starting activemq ... "
+${base_dir}/activemq/bin/activemq start
+echo "Starting thoth index ..."
+cd ${base_dir}/thoth-index ; nohup java -jar start.jar thoth &
+echo "Starting an example solr index for collection ... "
+cd ${base_dir}/collector ; nohup java -jar start.jar thoth &
