00c0189f95583dd143680eb364ce765933f30dc1
sgudla <sgudla@trulia.com>
2014-11-10 16:24:58 -0800

    BEST-1389 Created start & stop scripts

---
+++ start_demo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+base_dir=`pwd`
+# Start activeMQ first
+echo "Starting activemq ... "
+${base_dir}/activemq/bin/activemq start
+echo "Starting thoth index ..."
+cd ${base_dir}/thoth-index ; nohup java -jar start.jar &
+echo "Starting an example solr index for collection ... "
+cd ${base_dir}/to-be-intercepted ; nohup java -jar start.jar &
+echo "Starting Thoth ... "
+cd ${base_dir}/thoth ; nohup java -jar start.jar &
